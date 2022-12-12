8e9fc20c7991ec8a6a5ff3eb1b1032a5bab483a2
jungHyeonS <fbtls25@naver.com>
2022-12-12 19:40:55 +0900

    feat : todo setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 const Home = () => {
-    return "Home";
+    const [text,setText] = useState("");
+    const onChange = (e) => {
+        setText(e.target.value);
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setText("");
+    }
+    return (
+        <>
+            <h1>To Do</h1>
+            <form onSubmit={onSubmit}>
+                <input type="text" value={text} onChange={onChange}/>
+                <button>Add</button>
+            </form>
+            <ul></ul>
+        </>
+    )
 }
 
 export default Home;
